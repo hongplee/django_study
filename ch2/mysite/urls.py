5369fcc1d9dd675ca6cebaeddc030be0ae0ba899
hongplee <hongplee@ktoto.co.kr>
2017-02-07 15:07:56 +0900

    url 및 view 코딩
    url 및 view 코딩

--- ch2/mysite/urls.py
+++ ch2/mysite/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from bookmark.views import BookmarkLV, BookmarkDV
+#from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
     
-    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
-    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+    #url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    #url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
 ]
