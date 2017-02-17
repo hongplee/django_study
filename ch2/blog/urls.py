590046e5cda3fb648b380ff1996ed29383329b4b
hongplee <hongplee@ktoto.co.kr>
2017-02-17 12:37:30 +0900

    20170217 수정

--- ch2/blog/urls.py
+++ ch2/blog/urls.py
@@ -13,10 +13,9 @@ urlpatterns = [
     # /2012/
     url(r'^(?P<year>\d{4})/$', PostYAV.as_view(), name='post_archive_year'),
     # /2012/nov/
-    #url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', PostMAV.as_view(), name='post_archive_month'),
-    url(r'^(?P<year>\d{4})/\d{2}[가-힣]/$', PostMAV.as_view(), name='post_archive_month'),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', PostMAV.as_view(), name='post_archive_month'),
     # /2012/nov/10
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_archive_day'),
     # /today/
-    url(r'^today/$', PostTAV.as_view(), name='post_today_archive'),
+    url(r'^today/$', PostTAV.as_view(), name='post_archive_today'),
 ]
